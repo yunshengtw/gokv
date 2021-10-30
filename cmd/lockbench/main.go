a80b9b235e6bf9bd5d877009d66798cf21a7f6da
Yun-Sheng Chang <jiungsen@gmail.com>
2021-10-30 10:32:57 -0400

    Update lockbench

--- cmd/lockbench/main.go
+++ cmd/lockbench/main.go
@@ -25,7 +25,6 @@ func client(r int64, d time.Duration, coord memkv.HostName, c chan uint64) {
 		ck.Unlock(k)
 		x++
 	}
-	fmt.Printf("Done!\n")
 	c <- x
 }
 
@@ -37,9 +36,7 @@ func main() {
 	usage_assert := func(b bool) {
 		if !b {
 			flag.PrintDefaults()
-			fmt.Println("Must provide command in form:")
-			fmt.Println(" range RANGE")
-			fmt.Println(" duration DURATION")
+			fmt.Println("Must provide # keys, duration (ms), and # clients.")
 			os.Exit(1)
 		}
 	}
@@ -47,40 +44,28 @@ func main() {
 	usage_assert(coordStr != "")
 
 	coord := grove_ffi.MakeAddress(coordStr)
-	ck := lockservice.MakeLockClerk(coord, connman.MakeConnMan())
-	var _ = ck
 
-	n := 4
-	var r int64 = 10
-	var d time.Duration = 0
 	a := flag.Args()
-	usage_assert(len(a) > 0)
-	if a[0] == "range" {
-		usage_assert(len(a) >= 2)
-		v, err := strconv.ParseInt(a[1], 10, 64)
-		usage_assert(err == nil)
-		r = v
-	}
-	if a[2] == "duration" {
-		usage_assert(len(a) >= 4)
-		v, err := strconv.ParseInt(a[3], 10, 64)
-		usage_assert(err == nil)
-		d = time.Duration(v)
-	}
-
-	fmt.Printf("RANGE = %d; DURATION = %d\n", r, d)
+	usage_assert(len(a) == 3)
+	r, err := strconv.ParseInt(a[0], 10, 64)
+	usage_assert(err == nil)
+	d, err := strconv.ParseInt(a[1], 10, 64)
+	usage_assert(err == nil)
+	n, err := strconv.ParseInt(a[2], 10, 0)
+	usage_assert(err == nil)
 
 	done = false
 	ch := make(chan uint64, n)
-	for i := 0; i < n; i++ {
-		go client(r, d, coord, ch)
+	for i := int64(0); i < n; i++ {
+		go client(r, time.Duration(d), coord, ch)
 	}
-	time.Sleep(3 * time.Second)
+	time.Sleep(10 * time.Second)
 	done = true
 
-	for i := 0; i < n; i++ {
-		x := <- ch
-		fmt.Printf("x = %d.\n", x)
+	var x uint64 = 0
+	for i := int64(0); i < n; i++ {
+		x += <- ch
 	}
+	fmt.Printf("%d, %d, %d, %d\n", r, d, n, x / 10)
 }
 
